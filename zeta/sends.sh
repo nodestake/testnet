c243a09ad31042b9d1185bf8f9699cfc2d71d103
NodeStake <info@nodestake.top>
2025-05-23 07:16:58 +0800

    update

--- zeta/sends.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for t in {1..1100}
-do
-echo "RUN /$t"
-echo $pass | zetacored tx bank send mykey zeta1pgs0m6x6n6ddtger0ay9086xqt4chajsqyct3t 1azeta --yes --chain-id=athens_7001-1 --fees=2000azeta --note="NodeStake  1000 TXs+ task"
-sleep 10s
-done
